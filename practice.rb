3c1562154d725337e4e523b6f00ef56137257d93
crisandryj <crisandryj@gmail.com>
2023-01-03 18:25:23 -0500

    practiced puts

--- practice.rb
+++ practice.rb
@@ -5,3 +5,21 @@ puts "This is fun."
 puts 'Yay! Printing.'
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
+
+puts "I will now count my chickens:"
+
+puts "Hens", 100 - 25 * 3 % 4
+
+puts "Now I will count the eggs:"
+
+puts (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6).to_f
+puts "is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+puts "What is 3 +2?", 3 + 2
+puts "What is 5 - 7?", 5 -7
+puts "Ohm that's why it's false"
+
+puts "How about some more."
+
+puts " Is it greater", 5 > -2
