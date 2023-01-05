00a5df9bf518d3b304fa111e442777428096639f
crisandryj <crisandryj@gmail.com>
2023-01-04 19:35:42 -0500

    prompt practice

--- practice.rb
+++ practice.rb
@@ -1,25 +1,21 @@
-puts "Hello World!"
-puts "Hello Again"
-puts "I like typing this."
-puts "This is fun."
-puts 'Yay! Printing.'
-puts "I'd much rather you 'not'."
-puts 'I "said" do not touch this.'
+user = ARGV.first
+prompt = '>'
 
-puts "I will now count my chickens:"
+puts "Hi #{user}, I'm the #{$0} script."
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{user}?"
+print prompt
+likes = STDIN.gets.chomp()
 
-puts "Hens", 100 - 25 * 3 % 4
+puts "Where do you live #{user}?"
+print prompt
+lives = STDIN.gets.chomp()
+puts "What kind of computer do you have?"
+print prompt
+computer = STDIN.gets.chomp()
 
-puts "Now I will count the eggs:"
-
-puts (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6).to_f
-puts "is it true that 3 + 2 < 5 - 7?"
-
-puts 3 + 2 < 5 - 7
-puts "What is 3 +2?", 3 + 2
-puts "What is 5 - 7?", 5 -7
-puts "Ohm that's why it's false"
-
-puts "How about some more."
-
-puts " Is it greater", 5 > -2
+puts <<MESSAGE
+Alright, you said #{likes} about likeing me.
+You live in #{lives}. Not sure where that is.
+And you have a #{computer}. Nice.
+MESSAGE
