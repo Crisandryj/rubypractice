3ef2d469c8b246eb2d0b060803d31a738a5628af
crisandryj <crisandryj@gmail.com>
2023-01-08 17:55:58 -0500

    write in file

--- practice.rb
+++ practice.rb
@@ -11,10 +11,20 @@ STDIN.gets
 
 puts "Opening the file..."
 target = File.open(filename, "w")
-#puts
+
 puts "Truncating the file. Goodbye!"
 target.truncate(target.size)
-#puts
+
 puts "Now I'm going to ask you for three lines."
-#print
+
 print "line1: "; line1 = STDIN.gets.chomp()
+print "line 2: "; line2 = STDIN.gets.chomp()
+
+puts "I'm going to write these to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write("line3")
+target.write("\n")
