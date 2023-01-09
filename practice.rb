4df249694f40a78ec7e13a1160305a139d886f77
crisandryj <crisandryj@gmail.com>
2023-01-09 08:06:58 -0500

    rewrote function

--- practice.rb
+++ practice.rb
@@ -3,3 +3,8 @@ def puts_two(*args)
     arg1,arg2 = args
   puts "args1: #{arg1}, arg2: #{arg2}"
 end
+
+#ok, that *args is actually pointless, we can just do this
+def puts_two_again(arg1, arg2)
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
