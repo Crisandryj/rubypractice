e573cc52c07a68e350dd9d14f589fc0e0490f599
crisandryj <crisandryj@gmail.com>
2023-02-13 19:25:13 -0500

    count all numbers greater than 5

--- practice.rb
+++ practice.rb
@@ -1,6 +1,16 @@
 #practice
-array = [3,5,3,4,5]
+array = [3,5,3,4,5,6,7,8]
 
-def count
-  p 'hi'
+count = 0
+
+def count(array)
+  num = 0
+array.each do |n|
+  if n > 5
+    num += 1
+  end
+ end
+ p num
 end
+
+count(array)
