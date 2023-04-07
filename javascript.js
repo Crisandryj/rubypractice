8aa15f087b558bc035f22f0f5d8e4d7743f2d9cc
crisandryj <crisandryj@gmail.com>
2023-04-07 07:35:19 -0400

    added red color to text

--- javascript.js
+++ javascript.js
@@ -3,5 +3,6 @@ const container = document.querySelector('#container');
 const content = document.createElement('p');
 
 content.textContent = "Hey!, Im red"
+content.style.cssText = "color: red;"
 
 container.appendChild(content)
