faa3575f78211136a76437aa52b21a87eda2eafd
crisandryj <crisandryj@gmail.com>
2023-04-08 07:26:36 -0400

    added event listener to btn

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
 const btn = document.querySelector('#btn');
 btn.onclick = () => alert("Hello World");
 
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  alert("Hello World");
+});
+
 const container = document.querySelector('#container');
 
 const content = document.createElement('p');
