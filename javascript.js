636cfff890f8fb26105d8cb3f8e5c14614a4b37e
crisandryj <crisandryj@gmail.com>
2023-04-07 20:29:32 -0400

    onclick button

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+
+
 const container = document.querySelector('#container');
 
 const content = document.createElement('p');
@@ -7,15 +9,15 @@ content.style.cssText = "color: red;"
 
 
 const pink = document.createElement('div')
+
 pink.textContent = "hello"
-pink.setAttribute('style', 'border-style:solid; background: pink;')
+pink.setAttribute('style', 'border-style:solid; gbackground: pink;')
+const metoo = document.createElement('p')
+metoo.textContent = "me too!"
 
-
-
+pink.appendChild(metoo)
 const heading = document.createElement('h1')
-
 heading.textContent ="hi I'm a div"
 
 pink.appendChild(heading)
-
 container.appendChild(pink)
