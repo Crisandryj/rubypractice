ca9885e0205070cbc4aaba1b04f909636dcc8da1
crisandryj <crisandryj@gmail.com>
2023-04-09 09:30:17 -0400

    prac

--- javascript.js
+++ javascript.js
@@ -1,29 +1,5 @@
 const btn = document.querySelector('#btn');
-btn.onclick = () => alert("Hello World");
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', () => {
-  alert("Hello World");
+btn.addEventListener('click', function (e) {
+  e.target.style.background = 'blue';
 });
-
-const container = document.querySelector('#container');
-
-const content = document.createElement('p');
-
-content.textContent = "Hey!, Im red"
-content.style.cssText = "color: red;"
-
-
-const pink = document.createElement('div')
-
-pink.textContent = "hello"
-pink.setAttribute('style', 'border-style:solid; gbackground: pink;')
-const metoo = document.createElement('p')
-metoo.textContent = "me too!"
-
-pink.appendChild(metoo)
-const heading = document.createElement('h1')
-heading.textContent ="hi I'm a div"
-
-pink.appendChild(heading)
-container.appendChild(pink)
