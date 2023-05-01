969c5599c4416b50c611e3c1dab8593167a4398f
crisandryj <crisandryj@gmail.com>
2023-05-01 07:29:51 -0400

    added books to library

--- Book/javascript.js
+++ Book/javascript.js
@@ -9,3 +9,17 @@ function Book(title,author,pages) {
 function addBookToLibrary(book) {
     myLibrary.push(book)
 }
+
+let book1 = new Book('Go','Cris',145)
+let book2 = new Book('Carry','Javy',155)
+let book3 = new Book('Thrid','Jany',165)
+
+addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
+
+console.log(myLibrary)
+
+function displayBooks(){
+
+}
