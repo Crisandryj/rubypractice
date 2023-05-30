00be00a25192e989f413ef91f46948861ad785eb
crisandryj <crisandryj@gmail.com>
2023-05-30 07:02:20 -0400

    Fix: Print sum

--- Practice/practice.rb
+++ Practice/practice.rb
@@ -321,7 +321,7 @@ class PaginationHelper
 end
 
 helper = PaginationHelper.new([1,2,3,4,5,6],4)
-g
+
 def score( dice )
   hash = dice.group_by { |n| dice.count(n)}
 end
@@ -343,3 +343,43 @@ def two_sum(nums, target)
         end
     end
 end
+
+# Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+#
+# I can be placed before V (5) and X (10) to make 4 and 9.
+# X can be placed before L (50) and C (100) to make 40 and 90.
+# C can be placed before D (500) and M (1000) to make 400 and 900.
+# Given a roman numeral, convert it to an integer.
+
+
+def roman_to_int(s)
+  array = s.split("")
+  sum = 0
+    hash ={
+        'X' => 10,
+        'V' => 5,
+        'I' => 1,
+        'L' => 50,
+        'C' => 100
+    }
+    current_letter = ''
+    array.each_with_index do |l,index|
+      current_letter =  l
+      if current_letter == 'I' || current_letter == 'X' || current_letter == 'C'
+        if hash[array[index+1]] != nil
+          if hash[current_letter] > hash[array[index+1]]
+            sum += hash[array[index+1]] - hash[current_letter]
+          else
+            sum += hash[array[index+1]] + hash[current_letter]
+          end
+        else
+          sum+= hash[current_letter]
+        end
+      
+      end
+      p sum
+ end
+
+string = "XXLV"
+
+ roman_to_int(string)
